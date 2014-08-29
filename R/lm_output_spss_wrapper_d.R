857be262dd68dfd10b1105ad7db7076c4525f082
lillion <lillion@lillions-MacBook-Pro.local>
2014-08-29 08:39:48 +0200

    del readlines

--- R/lm_output_spss_wrapper_d.R
+++ R/lm_output_spss_wrapper_d.R
@@ -1,16 +1,22 @@
 #' @title Ergebnis einer linearen Regression ähnlich SPSS
 #'
-#' @description <full description>
+#' @description Dreigeteiltes Ergebnis einer linearen Regression ähnlich der Ausgabe in SPSS.  Neben einem Modelübersicht mit Varianzaufklärung werden die Koeffizienten aufgeführt und eine Kollinearitätsdiagnose angeboten.
 #' @param fit Objekt eines linearen Modells
 #' @param plot sollen Histogramm der Residuen, qq plot und Plot dervorhergesagten Werte/Fehler erstellt werden
 #' @export
 #' @keywords lm, regression, spss
-#' @return dataframe des Ergebnisses einer Regression ähnlich der in SPSS
+#' @return Liste mit 3 Elementen (dataframe) des Ergebnisses einer Regression ähnlich der in SPSS
 #' @seealso \code{\link{lm}}
 #' @examples
 #' library(car)
 #' fit <- lm(mpg~disp+hp+wt+drat, data=mtcars) 
 #' lm_output_spss_wrapper_d(fit)
+#' 
+#' \dontrun{
+#' # um "schöne" Tabellen zu bekommen
+#' kable(lm_output_spss_wrapper_d(l))
+#' lapply(ll, function(x) kable(lm_output_spss_wrapper_d(x)))
+#' }
 lm_output_spss_wrapper_d <- function (fit, Rsquare=TRUE, coefficients=TRUE, collinearity=TRUE,plot=TRUE) {
   if(class(fit)!="lm") stop(substitute(fit), " ist kein LM Objekt")
   #lm_hier_model_sum(fit)
