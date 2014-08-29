d7b8e0d32d161cce2df73c38bf3e0bd28bd2a250
lillion <lillion@lillions-MacBook-Pro.local>
2014-08-29 08:41:41 +0200

    cleaned spaces

--- R/simplebootstrap_d.r
+++ R/simplebootstrap_d.r
@@ -14,17 +14,17 @@
 #' simpleboot_d(mydata, sd, reps=5000)
 #'}
 simpleboot_d<-function(x,stat,reps=1000) {
- cat("This simple function will not show you warning messages.\n")
- cat("Check results closely and be prepared to consult a statistician.\n")
- stat <- as.character(substitute(stat))
- if(stat=="max" | stat=="min") { warning("Bootstrap is likely to fail for minima and maxima") }
- require(boot)
- eval(parse(text=eval(substitute(paste("p.func<-function(x,i) ",stat,"(x[i])",sep=""),list(stat=stat)))))
- myboots<-boot(x,statistic=p.func,R=reps,stype="i")
- hist(myboots$t,breaks=25,main="Empirische Verteilung des bootstrap",xlab=paste("Statistik:",stat),ylab="Häufigkeit")
- abline(v=mean(myboots$t),col="blue")
- abline(v=myboots$t0, col="red")
- suppressWarnings(return(list(Anzahl_Samples=reps,Punkt.Schätzung=myboots$t0,normal.ci=c(boot.ci(myboots)$normal[2],boot.ci(myboots)$normal[3]),
+cat("This simple function will not show you warning messages.\n")
+cat("Check results closely and be prepared to consult a statistician.\n")
+stat <- as.character(substitute(stat))
+if(stat=="max" | stat=="min") {warning("Bootstrap is likely to fail for minima and maxima")}
+require(boot)
+eval(parse(text=eval(substitute(paste("p.func<-function(x,i) ",stat,"(x[i])",sep=""),list(stat=stat)))))
+myboots<-boot(x,statistic=p.func,R=reps,stype="i")
+hist(myboots$t,breaks=25,main="Empirische Verteilung des bootstrap",xlab=paste("Statistik:",stat),ylab="Häufigkeit")
+abline(v=mean(myboots$t),col="blue")
+abline(v=myboots$t0, col="red")
+suppressWarnings(return(list(Anzahl_Samples=reps,Punkt.Schätzung=myboots$t0,normal.ci=c(boot.ci(myboots)$normal[2],boot.ci(myboots)$normal[3]),
 percent.ci=c(boot.ci(myboots)$percent[4],boot.ci(myboots)$percent[5]),
 bca.ci=c(boot.ci(myboots)$bca[4],boot.ci(myboots)$bca[5]))))
 }
