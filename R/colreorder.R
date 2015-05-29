2961742e31fd7f910b57a0b9984d40625dc12999
lillion <lillion@lillions-MacBook-Pro.local>
2015-05-29 14:07:26 +0200

    psych import added

--- R/colreorder.R
+++ R/colreorder.R
@@ -19,4 +19,25 @@ return( data[, c(refcols, setdiff(names(data), refcols))])
 
 
 
+#' Variablen-Reihenfolge ändern
+#'
+#' @description für ein simples Umstellen der Variablenreihenfolge im Datensatz
+#' @param variablen columns to put at the left side or right side of a dataframe
+#' @param data Datensatz
+#'
+#' @return a dataframe with the reordered columns
+#' @export
+#'
+#' @examples
+#' data(crime_data)
+#' temp <- varumstellen(c("crime","murder"), crime_data)
+#' head(temp)
+#' rm(temp)
+varumstellen <- function(variablen, data) {
+  if (!class(variablen)=="character") stop("columns need to be given as strings")
+  return( data[, c(refcols, setdiff(names(data), refcols))])
+}
+
+
+
 
--- R/row.has.na.R
+++ R/row.has.na.R
@@ -1,8 +1,8 @@
-#' Missinganalyse pro Zeile
+#' Analyse cases for missings
 #'
-#' @param data Datensatz
-#' @param which logisch: sollen nur die Fallnummern ausgegeben werden
-#' @param Zeile logisch: Missings pro Zeile anzeigen
+#' @param data dataframe
+#' @param which logical: sollen nur die Fallnummern ausgegeben werden
+#' @param Zeile logical: Missings pro Zeile anzeigen
 #' 
 #'
 #' @return Fallnummern bzw. Ja/Nein
@@ -16,5 +16,27 @@ row.has.na <- function (data, which=TRUE, zeile=FALSE) {
   r <- apply(data,1, function(x) {sum(is.na(x))})
   if(zeile) return(r)
   if(which) return(which(r!=0))
+  return(r!=0)
+}
+
+
+#' Missinganalyse pro Zeile
+#'
+#' @param data Datensatz
+#' @param which logisch: sollen nur die Fallnummern ausgegeben werden
+#' @param Zeile logisch: Missings pro Zeile anzeigen
+#' 
+#'
+#' @return Fallnummern bzw. Ja/Nein
+#' @export
+#'
+#' @examples
+#' demoframe <- data.frame(sapply(1:15,function(x) rnorm(50,100,10)))
+#' demoframe <- as.data.frame(lapply(demoframe, function(x) "is.na<-"(x, sample(seq(x), floor(length(x) * runif(1, 0, .2))))))
+#' fall.hat.missings(demoframe)
+fall.hat.missings <- function (data, welcher=TRUE, zeile=FALSE) {
+  r <- apply(data,1, function(x) {sum(is.na(x))})
+  if(zeile) return(r)
+  if(welcher) return(which(r!=0))
   return(r!=0)
 }
