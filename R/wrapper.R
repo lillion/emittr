9821fbdecb20dba70456bea3966ce46a8e271c22
lillion <lillion@lillions-MBP.fritz.box>
2016-01-31 15:52:53 +0100

    Radarplot + odds

--- R/wrapper.R
+++ R/wrapper.R
@@ -27,3 +27,37 @@ tprozent <- function(twert, SW=FALSE){
   prozent <- pnorm(z)*100
   cat("T Wert:",twert, "\nz Wert:",z,"\nProzent:", prozent)
 }
+
+#' Odds zu Wahrscheinlichkeiten umwandeln
+#'
+#' @param odds einzeln oder als Vektor oder als glm Objekt
+#'
+#' @return p Wert
+#' @export
+#'
+#' @examples
+#' odds2p(c(1,2,.5))
+odds2p <- function(odds) {
+  if(odds$family[1]=="binomial") {
+    odds <- exp(coef(odds))
+      }
+  p <- odds/(odds + 1)    
+  p[is.nan(p)] <- 1
+  p}
+
+
+
+#' Wahrscheinlichkeiten (p) in Odds umwandeln
+#'
+#' @param p 
+#'
+#' @return Odds
+#' @export
+#'
+#' @examples
+#' p2odds(c(.10,.50,.90))
+p2odds <- function(p) {p/(1 - p)}
+
+
+
+
