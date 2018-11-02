0822a6d8d9dcb4a7b8b36b360159442a6cd7ec84
lillion <lillion@lillions-MacBook-Pro.local>
2018-11-02 14:40:22 +0100

    packages angepasst

--- R/installieren_notwendiger_packages.R
+++ R/installieren_notwendiger_packages.R
@@ -1,9 +1,10 @@
 #' @title Installieren notwendiger packages
 #'
 #' @description voellig unnoetige Funktion, aber praktisch
-#' "car", "lmtest", "QuantPsyc", "lavaan", 
-#' "semTools", "semPlot","GPArotation", "HH", "knitr",
-#'  "sjPlot", "pander", "lattice",  "ggvis","ez","dplyr"
+#' "car", "lmtest", "QuantPsyc", "lavaan", "lme4",
+#' "semTools", "semPlot","GPArotation", "HH", "knitr", "emmeans",
+#' "rockchalk"
+#'  "sjPlot", "pander", "lattice",  "ggvis","ez","tidyverse", "haven"
 #' früher noch: "psych", "ggplot2", "reshape2", "Hmisc", "perturb",
 #' "Rcmdr"
 #' @param update logisch: sollen gleichzeitig alle Pakete aktualisiert werden
@@ -13,7 +14,9 @@
 #' @examples 
 #' pakete_installieren()
 pakete_installieren <- function () {
-  wants <- c("car", "lmtest", "QuantPsyc", "lavaan", "psych", "ggplot2", "reshape2", "semTools", "semPlot","GPArotation", "HH", "Hmisc", "knitr", "sjPlot", "pander", "lattice", "perturb", "ggvis","ez","dplyr")
+  wants <- c("car", "lmtest", "QuantPsyc", "lavaan", "lme4", "haven","readxl","psycho", "rockchalk",
+             "psych", "ggplot2", "ggpubr", "GGally", "reshape2", "semTools", "semPlot","GPArotation", "emmeans",
+             "HH", "Hmisc", "knitr", "sjPlot", "pander", "lattice", "perturb", "ggvis","ez","tidyverse")
   has   <- wants %in% rownames(installed.packages())
   if(any(!has)) install.packages(wants[!has],dependencies=T)
   update.packages(ask = F)
